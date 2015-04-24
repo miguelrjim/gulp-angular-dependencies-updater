2c5f84138732fc011e6e7821d79ddf69ed8a0f04
Miguel Jimenez <mjimenez@rocketlawyer.com>
2015-04-23 17:26:21 -0700

    fixing multiple issues, first working version

--- index.js
+++ index.js
@@ -5,21 +5,18 @@ var nedb = require('nedb');
 var vinyl_fs = require('vinyl-fs');
 
 module.exports = function (options) {
-	if (!options.db) {
-		throw new gutil.PluginError('gulp-angular-dependencies-updater', '`db` required');
-	}
+  if (!options.db) {
+    throw new gutil.PluginError('gulp-angular-dependencies-updater', '`db` required');
+  }
 
-	// Load specified database
-	var db = new nedb({
-		filename: options.db,
-		autoload: true
-	});
+  // Load specified database
+  var db = options.db;
 
   var filesToAdd = [];
   var dependenciesAdded = {};
 
-	return through.obj(function (file, enc, cb) {
-    this.push(file.path);
+  return through.obj(function (file, enc, cb) {
+    this.push(file);
 
     var dependencies = []
 
@@ -27,9 +24,10 @@ module.exports = function (options) {
       path: file.path.substr(process.cwd().length+1)
     }, function(err, docs) {
       docs.forEach(function(doc) {
-        if(!dependenciesAdded[doc.name] && doc.dependents && doc.dependents.length > 0)
-          dependencies = dependencies.concat(doc.dependents);
-        dependenciesAdded[doc.name] = true;
+        if(!dependenciesAdded[doc.name]) {
+          dependencies.push(doc.name);
+          dependenciesAdded[doc.name] = true;
+        }
       });
       addDependencies(function() {
         cb();
@@ -38,30 +36,33 @@ module.exports = function (options) {
 
     function addDependencies(fn) {
       if(dependencies.length == 0) {
-        return fn();
+        fn();
+        return;
       }
       db.find({
-        name: {
+        dependencies: {
           $in: dependencies
         }
       }, function(err, docs) {
         dependencies = [];
         docs.forEach(function(doc) {
-          if(!dependenciesAdded[doc.name] && doc.dependents && doc.dependents.length > 0)
-            dependencies = dependencies.concat(doc.dependents);
-          dependenciesAdded[doc.name] = true;
+          if(!dependenciesAdded[doc.name]) {
+            dependencies.push(doc.name);
+            dependenciesAdded[doc.name] = true;
+          }
           filesToAdd.push(doc.path);
         });
         addDependencies(fn);
       })
     }
-	}, function(cb) {
+  }, function(cb) {
     var that=this;
     vinyl_fs.src(filesToAdd, {
       read: false
     })
       .pipe(through.obj(function(file, enc, cb) {
         that.push(file);
+        cb();
       }, function(icb) {
         icb();
         cb();
